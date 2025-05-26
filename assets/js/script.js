a738743133c2d15b78ce8dfd96e3001a402bcd16
Birdnana <julie15190723@gmail.com>
2025-05-26 18:04:34 +0800

    feat: 新增回頂部按鈕功能

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
 $(window).on('load', function () {
-    $('.preloader').fadeOut(100);
+  $('.preloader').fadeOut(100);
+});
+
+// 網頁title
+$(function () {
+  document.title = $('h1').text();
+});
+
+// 回頂部按鈕
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollTopBtn').fadeIn();
+    } else {
+      $('.scrollTopBtn').fadeOut();
+    }
+  });
+
+  $('.scrollTopBtn').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 0);
+    return false;
+  });
 });
